36b93504e63924b43e21ed1a904f680015bcede8
anjali gupta <63493170+anjaligupta-hash@users.noreply.github.com>
2021-04-17 14:58:47 +0530

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+<div class="row">
+<div class="col-sm-4 col-sm-offset-4">
+<form action="login" method="POST">
+<div class="form-group">
+   @csrf
+<label for="exampleInputEmail">Email Address</label>
+<input type="email" name="email" class="form-control" id="exampleInputEmail"> </div>
+<div class="form-group">
+<label for="exampleInputPassword">Password</label>
+<input type="password" name="password" class="form-control" id="exampleInputPassword">
+</div>
+<button type="submit" class="btn btn-default">Login</button>
+</form>
+</div>
+</div>
+</div>
+@endsection()
